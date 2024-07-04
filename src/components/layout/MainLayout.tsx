8a7ade0fd2ddbde41ab111024be1c0e86a3192b0
Kazi Mehedi Hasan <hassan.mehedee92@gmail.com>
2024-07-04 12:07:07 +0600

    antd basic routing setup and code refactor

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,80 @@
+import { Layout, Menu } from "antd";
+import { Outlet } from "react-router-dom";
+import { adminSidebarRoutes } from "../../routes/admin.routes";
+const { Header, Content, Footer, Sider } = Layout;
+
+// const items = [
+//   {
+//     key: "Dashboard",
+//     label: <NavLink to='/admin/dashboard'>Dashboard</NavLink>,
+//   },
+//   {
+//     key: "User Management",
+//     label: 'User Management',
+//     children: [
+//       {
+//         key: 'Create Admin',
+//         label: <NavLink to='/admin/create-admin'>Create Admin</NavLink>
+//       },
+//       {
+//         key: 'Create Faculty',
+//         label: <NavLink to='/admin/create-faculty'>Create Faculty</NavLink>
+//       },
+//       {
+//         key: 'Create Student',
+//         label: <NavLink to='/admin/create-student'>Create Student</NavLink>
+//       },
+//     ]
+//   }
+
+// ];
+
+export const MainLayout = () => {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div style={{
+          color: "white",
+          height: '4rem',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+          <h1 style={{ height: '20px' }}>PH University</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['4']}
+          items={adminSidebarRoutes}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            {/* //! This is where main content will go */}
+            <Outlet />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  )
+}
---
+++ src/routes/admin.routes.tsx
@@ -0,0 +1,115 @@
+import { ReactNode } from "react";
+import AdminDashboard from "../pages/admin/AdminDashboard";
+import CreateAdmin from "../pages/admin/CreateAdmin";
+import CreateFaculty from "../pages/admin/CreateFaculty";
+import CreateStudent from "../pages/admin/CreateStudent";
+import { NavLink } from "react-router-dom";
+
+type TAntPath = {
+    key: string;
+    label: ReactNode;
+    children?: TAntPath[];
+};
+
+type TReactRoute = {
+    path: string;
+    element: ReactNode;
+}
+const adminPaths = [
+    {
+        name: 'Dashboard',
+        path: 'dashboard',
+        element: <AdminDashboard />
+    },
+    {
+        name: 'User Management',
+        children: [
+            {
+                name: 'Create Admin',
+                path: 'create-admin',
+                element: <CreateAdmin />
+            },
+            {
+                name: 'Create Faculty',
+                path: 'create-faculty',
+                element: <CreateFaculty />
+            },
+            {
+                name: 'Create Student',
+                path: 'create-student',
+                element: <CreateStudent />
+            },
+        ]
+    }
+]
+
+export const adminRoutes = adminPaths.reduce((acc: TReactRoute[], item) => {
+    if (item.path && item.element) {
+        acc.push({
+            path: item.path,
+            element: item.element
+        });
+    }
+    if (item.children) {
+        item.children.forEach((child) => {
+            acc.push({
+                path: child.path,
+                element: child.element
+            })
+        })
+    }
+    return acc;
+}, [])
+
+export const adminSidebarRoutes = adminPaths.reduce((acc: TAntPath[], val) => {
+    if (!val['children']) {
+        acc.push({
+            key: val.name,
+            label: <NavLink to={`/admin/${val.path}`}>{val.name}</NavLink>
+        })
+    } else {
+        acc.push({
+            key: val.name,
+            label: val.name,
+            // children: val.children.reduce((initialState: TAntPath[], child) => {
+            //     initialState.push({
+            //         key: child.name,
+            //         label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>
+            //     })
+            //     return initialState
+            // }, [])
+            children: val.children.map((child) => {
+                return ({
+                    key: child.name,
+                    label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>
+                })
+            })
+        })
+    }
+    return acc;
+}, [])
+
+//^ hard coded way
+// export const adminPaths = [
+//     {
+//         // path: 'dashboard',
+//         index: true,
+//         element: <AdminDashboard />
+//     },
+//     {
+//         path: 'dashboard',
+//         element: <AdminDashboard />
+//     },
+//     {
+//         path: 'create-student',
+//         element: <CreateStudent />
+//     },
+//     {
+//         path: 'create-faculty',
+//         element: <CreateFaculty />
+//     },
+//     {
+//         path: 'create-admin',
+//         element: <CreateAdmin />
+//     }
+// ]
