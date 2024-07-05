b540a0b0b651fa6a00d1fa3acd52859e9d872500
Kazi Mehedi Hasan <hassan.mehedee92@gmail.com>
2024-07-05 23:00:11 +0600

    logout and redirect

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,14 @@
 import { Button } from "antd";
-import { useForm } from "react-hook-form"
+import { FieldValues, useForm } from "react-hook-form"
 import { authApi } from "../redux/Features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/Features/auth/authSlice";
+import { setUser, TUser } from "../redux/Features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 export const Login = () => {
+    const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const { register, handleSubmit } = useForm({
         defaultValues: {
@@ -13,21 +16,28 @@ export const Login = () => {
             password: 'admin123'
         }
     });
-    const [login, { error }] = authApi.useLoginMutation();
+    const [login] = authApi.useLoginMutation();
 
     // console.log('data =>', data);
     // console.log('error =>', error);
 
-    const onSubmit = async (data) => {
-        const userInfo = {
-            id: data.userId,
-            password: data.password
-        }
-        const res = await login(userInfo).unwrap();
+    const onSubmit = async (data: FieldValues) => {
+        const toastId = toast.loading('Logging in');
+        try {
+            const userInfo = {
+                id: data.userId,
+                password: data.password
+            }
+            const res = await login(userInfo).unwrap();
 
-        const user = verifyToken(res.data.accessToken);
-        // console.log(user);
-        dispatch(setUser({ user, token: res.data.accessToken }));
+            const user = verifyToken(res.data.accessToken) as TUser;
+            // console.log(user);
+            dispatch(setUser({ user, token: res.data.accessToken }));
+            navigate(`/${user.role}/dashboard`);
+            toast.success('Logged in successfully', { id: toastId, duration: 3000 });
+        } catch (error) {
+            toast.error(`${error.data.message}`, { id: toastId, duration: 3000 });
+        }
     }
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
